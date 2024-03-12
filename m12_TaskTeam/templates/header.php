59e2adcf1e20b4ad8a68efac17d54c5a11cb07ff
JuanCumbe_Local <juanfernandocumbe@gmail.com>
2024-03-12 16:54:14 +0100

    update

--- m12_TaskTeam/templates/header.php
+++ m12_TaskTeam/templates/header.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION["mail"])) {
 <head>
   <title>TaskTeam</title>
   <!-- Required meta tags -->
-  <meta charset="utf-8">
+  <meta charset="uft-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
